c2e1a194621fcae7c0fc3fc861ce798c77bbc79f
潘健森 <sennes111762@gmail.com>
2014-06-27 18:26:05 +0800

    uodate

---
+++ led.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:16:48 06/27/2014 
+// Design Name: 
+// Module Name:    led 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module led(
+    );
+
+
+endmodule
--- top.v
+++ top.v
@@ -1,14 +1,16 @@
 `timescale 1ns / 1ps
 
-module top(clk,rst,ds18b20,the_btn,SEG7_SEG,SEG7_SEL,ADC549_DATA,ADC549_CLK,ADC549_CS_N);
+module top(clk,rst,ds18b20,the_btn,SEG7_SEG,SEG7_SEL,ADC549_DATA,ADC549_CLK,ADC549_CS_N,led);
 
 input  clk,rst,the_btn,ADC549_DATA;
 inout  ds18b20;
 output ADC549_CLK,ADC549_CS_N;
+output [7:0] led;
 output [7:0] SEG7_SEG;
 output [3:0] SEG7_SEL;
 
 wire [15:0] t_buf_inside;
+reg [7:0] led;
 
 ds18b20_drive ds18b20_0(
 	.clk(clk),
@@ -19,6 +21,9 @@ ds18b20_drive ds18b20_0(
 wire   [7 :0] ad_data;
 wire   [15:0] segdata;
 wire   [11:0] wire_bcd;
+wire   [7 :0] change;
+
+assign change = 8'b11111111-ad_data;
 assign segdata = {4'b0000,wire_bcd};
 tlc549_drive tlc549(
     .clk              (clk),
@@ -30,7 +35,7 @@ tlc549_drive tlc549(
 
 BCD bcd(
     .clk              (clk),
-    .DATA_IN          (ad_data),
+    .DATA_IN          (change),
     .DATA_OUT         (wire_bcd));
 
 wire xd_btn;
@@ -64,4 +69,21 @@ seg_drive seg(
   .o_seg            (SEG7_SEG),
   .o_sel            (SEG7_SEL)
 );
+
+
+always @(*) begin
+	led = 0;
+	if (rst) begin
+	led = 0;		
+	end
+	else if (change>8'b11011111) begin led[7:0] = 8'b11111111; end
+	else if (change>8'b10111111) begin led[6:0] = 7'b1111111; end
+	else if (change>8'b10011111) begin led[5:0] = 6'b111111; end
+	else if (change>8'b01111111) begin led[4:0] = 5'b11111; end
+	else if (change>8'b01011111) begin led[3:0] = 4'b1111; end
+	else if (change>8'b00111111) begin led[2:0] = 3'b111; end
+	else if (change>8'b00011111) begin led[1:0] = 2'b11; end
+	else if (change>8'b00000010) begin led[0:0] = 1'b1; end
+	else led = 1'b0;
+end
 endmodule
